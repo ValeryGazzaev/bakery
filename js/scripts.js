596b766bc2273bdc096daa1e75c5bad47d2a55c3
John Doe <uncolourfulf116@gmail.com>
2019-12-13 13:35:09 +0300

    prod start

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*jQuery(function($){
+    $(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $(".header-menu"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.hide(); // скрываем его
+        }
+    });
+    $('#header-menu-btn').click(function(){
+        $('.header-menu').toggle('fast')
+    })
+});*/
+
+/*код не рабочий так как не предусматривает расширение окна*/
