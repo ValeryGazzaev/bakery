75908ea09a736f180c8f75f4bd8376d452bda2c4
John Doe <uncolourfulf116@gmail.com>
2019-12-16 13:19:44 +0300

    next started undone

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 /*jQuery(function($){
     $(document).mouseup(function (e){ // событие клика по веб-документу
         var div = $(".header-menu"); // тут указываем ID элемента
-        if (!div.is(e.target) // если клик был не по нашему блоку
+        if ($(window).width() <= 1150) {
+            if (!div.is(e.target) // если клик был не по нашему блоку
             && div.has(e.target).length === 0) { // и не по его дочерним элементам
             div.hide(); // скрываем его
-        }
-    });
+    } 
+}
+
+});
     $('#header-menu-btn').click(function(){
         $('.header-menu').toggle('fast')
     })
